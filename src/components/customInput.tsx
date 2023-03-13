dcce7009530c75627e7dd5577f5fcc18cd3f2eac
RSalminen <robin.salminen@aalto.fi>
2023-03-13 20:35:34 +0200

    showpassword to login form

--- src/components/customInput.tsx
+++ src/components/customInput.tsx
@@ -1,5 +1,5 @@
 
-const CustomInput = ({labelText, onChange, currentValue, password}:{labelText:string, onChange:Function, currentValue:string, password?:boolean}) => {
+const CustomInput = ({labelText, onChange, currentValue, showPassword}:{labelText:string, onChange:Function, currentValue:string, showPassword?:boolean}) => {
 
     const valueChanged = (e:React.ChangeEvent<HTMLInputElement>) => {
         onChange(e);
@@ -12,7 +12,7 @@ const CustomInput = ({labelText, onChange, currentValue, password}:{labelText:st
     return (
         <div className="flex flex-col border-b-2 border-emerald-700 bg-gray-200 pb-0.5 rounded-t-sm w-full">
             <h5 className="text-[12px] text-emerald-800 pl-2">{labelText}</h5>
-            <input className="text-[13px] focus:outline-none bg-gray-200 px-1.5" type={password ? "password" : "text"} onKeyPress={stopPropagation} onKeyUp={stopPropagation} onKeyDown={stopPropagation} onChange={valueChanged} value={currentValue} />
+            <input className="text-[13px] focus:outline-none bg-gray-200 px-1.5" type={showPassword ? "text" : "password"} onKeyPress={stopPropagation} onKeyUp={stopPropagation} onKeyDown={stopPropagation} onChange={valueChanged} value={currentValue} />
         </div>
     )
 }
