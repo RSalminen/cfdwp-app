25d9ec6d918d3ec6b3d4c25006226f53c5a1e23d
RSalminen <robin.salminen@aalto.fi>
2023-03-13 19:54:33 +0200

    login form improvements

--- src/components/buttonDarkMid.tsx
+++ src/components/buttonDarkMid.tsx
@@ -2,10 +2,10 @@
 const ButtonDarkMid = ({btnText, onClickFn, fullWidth, deactive=false}:{btnText:string, onClickFn:Function, fullWidth:boolean, deactive?:boolean}) => {
 
     return (
-        <div className={`${deactive ? "bg-gray-700" : "bg-emerald-900 hover:cursor-pointer"} text-[14px] rounded-[3px] px-3 py-1 text-white flex justify-center items-center h-fit ${fullWidth ? "w-full" : "w-fit"}`}
+        <button className={`${deactive ? "bg-gray-700" : "bg-emerald-900 hover:cursor-pointer"} text-[14px] rounded-[3px] px-3 py-1 text-white flex justify-center items-center h-fit ${fullWidth ? "w-full" : "w-fit"}`}
             onClick={(e) => !deactive && onClickFn(e)}>
             {btnText}
-        </div>
+        </button>
     );
 }
 
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,29 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom";
-import ButtonDark from "../components/buttonDark";
-import ButtonDarkMid from "../components/buttonDarkMid";
 import CustomInput from "../components/customInput";
 import { userService } from "../services/userService";
+
+const ButtonDarkForm = ({btnText, fullWidth}:{btnText:string, fullWidth:boolean}) => {
+
+    return (
+        <button type="submit" className={`bg-emerald-900 text-[15px] rounded-[3px] px-3 py-2 text-white flex justify-center items-center hover:cursor-pointer h-fit ${fullWidth ? "w-full" : "w-fit"}`}>
+            {btnText}
+        </button>
+    );
+}
+
 
 const Login = () => {
 
     const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
+    const [showPassword, setShowPassword] = useState<boolean>(false);
+
     const navigate = useNavigate();
 
-    const loginClicked = async () => {
+    const loginClicked = async (e:any) => {
+        e.preventDefault();
         const loggedUser = await userService.login(username, password);
         if (loggedUser) {
             window.localStorage.setItem('loggedCFDWPUser', JSON.stringify(loggedUser));
@@ -27,15 +38,15 @@ const Login = () => {
     return (
         <div className="h-[100vh] w-full flex justify-center items-center">
             <div className="border py-6 px-10 w-[280px] rounded-md shadow-md shadow-gray-400">
-                <div className="flex flex-col space-y-3">
+                <form onSubmit={loginClicked} className="flex flex-col space-y-3">
                     <h3 className="text-center text-[20px] font-semibold mb-2">Login</h3>
                     <CustomInput currentValue={username} labelText="username" onChange={(e:any) => setUsername(e.target.value)} />
-                    <CustomInput currentValue={password} labelText="password" onChange={(e:any) => setPassword(e.target.value)} />
+                    <CustomInput currentValue={password} labelText="password" onChange={(e:any) => setPassword(e.target.value)} password={true} />
                     <div className="pt-1 flex flex-col space-y-0.5">
-                        <ButtonDark btnText="Log in" fullWidth={true} onClickFn={loginClicked} />
+                        <ButtonDarkForm btnText="Log in" fullWidth={true} />
                         <div className="text-end text-[13px]">Forgot password?</div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     )
