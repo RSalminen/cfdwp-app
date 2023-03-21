62d5e3cdffe8d69b3023d29581501b0ead039fc8
RobinSalminen <robin.salminen@aalto.fi>
2023-03-21 20:52:57 +0200

    add collections

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -26,7 +26,7 @@ const Home = () => {
         <img transition-style="in:wipe:right" role="presentation" className="h-full w-full fixed object-cover -z-10 top-0 left-0 pointer-events-none" src="wavebg1.svg"/>
         <div className="fixed w-full h-full top-0 left-0 -z-[11] backgroundcolor"></div>
 
-        <div className="w-[95%] sm:w-[80%] flex flex-col space-y-44">
+        <div className="w-[95%] sm:w-[80%] flex flex-col space-y-44 mb-10d">
           <div className="flex justify-between items-center">
 
             <img width={isMobile ? 200 : 320} src="/cfdviewernew.svg" alt="CFD Viewer logo" />
@@ -39,7 +39,8 @@ const Home = () => {
             </Link>
           </div>
 
-          <div className="flex-grow">
+          <div className="flex-grow flex flex-col space-y-8">
+            
             <div className="fade-in-card w-full bg-white shadow-md shadow-gray-600 rounded-md px-7 py-4 flex flex-col space-y-4 items-center sm:items-start">
               <h3 className="text-[22px] font-semibold">
                 Simulations
@@ -50,15 +51,36 @@ const Home = () => {
                     <svg className="h-8 w-8 fill-gray-600" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M113.5 281.2v85.3L256 448l142.5-81.5v-85.3L256 362.7l-142.5-81.5zM256 64L32 192l224 128 183.3-104.7v147.4H480V192L256 64z"></path></svg>
                   </div>
                   <div className="flex justify-center items-center px-2 h-[40%] w-full overflow-hidden text-center">
-                    <p className='text-[14px] inline-block break-words w-full font-medium'>This is a simulation</p>
+                    <p className='text-[14px] inline-block break-words w-full font-medium'>Simulation 1</p>
                   </div>
                 </div>
               </div>
               <div className="w-full flex justify-center text-center items-center space-x-1 text-blue-900 hover:cursor-pointer">
-                <p>Show more</p>
+                <p className="text-[13px]">Show more</p>
                 <svg className="h-[14px] w-[14px] mt-0.5" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M884 256h-75c-5.1 0-9.9 2.5-12.9 6.6L512 654.2 227.9 262.6c-3-4.1-7.8-6.6-12.9-6.6h-75c-6.5 0-10.3 7.4-6.5 12.7l352.6 486.1c12.8 17.6 39 17.6 51.7 0l352.6-486.1c3.9-5.3.1-12.7-6.4-12.7z"></path></svg>
               </div>
             </div>
+
+            <div className="fade-in-card w-full bg-white shadow-md shadow-gray-600 rounded-md px-7 py-4 flex flex-col space-y-4 items-center sm:items-start mb-5">
+              <h3 className="text-[22px] font-semibold">
+                Collections
+              </h3>
+              <div className="flex flex-wrap">
+                <div className="flex flex-col h-36 w-60 sm:h-32 sm:w-52 border rounded-md md:hover:cursor-pointer md:hover:-translate-y-2 transition-all">
+                  <div className="h-[60%] w-full bg-gray-200 flex justify-center items-center">
+                    <svg className="h-6 w-6 fill-gray-600" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><rect width="16" height="10" rx="1.5" transform="matrix(1 0 0 -1 0 14.5)"></rect><path fill-rule="evenodd" d="M2 3a.5.5 0 00.5.5h11a.5.5 0 000-1h-11A.5.5 0 002 3zm2-2a.5.5 0 00.5.5h7a.5.5 0 000-1h-7A.5.5 0 004 1z" clip-rule="evenodd"></path></svg>
+                  </div>
+                  <div className="flex justify-center items-center px-2 h-[40%] w-full overflow-hidden text-center">
+                    <p className='text-[14px] inline-block break-words w-full font-medium'>Collection 1</p>
+                    <svg className="h-[14px] w-[14px] mt-0.5" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M884 256h-75c-5.1 0-9.9 2.5-12.9 6.6L512 654.2 227.9 262.6c-3-4.1-7.8-6.6-12.9-6.6h-75c-6.5 0-10.3 7.4-6.5 12.7l352.6 486.1c12.8 17.6 39 17.6 51.7 0l352.6-486.1c3.9-5.3.1-12.7-6.4-12.7z"></path></svg>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full flex justify-center text-center items-center space-x-1 text-blue-900 hover:cursor-pointer">
+                <p className="text-[13px]">Show more</p>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
