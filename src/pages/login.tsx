36a7ae18dd97b5fce741a7c26fe51481aa21bb65
RSalminen <robin.salminen@aalto.fi>
2023-04-09 20:13:36 +0300

    fix for mobile

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -46,13 +46,13 @@ const Login = () => {
             <img role="presentation" className="h-full w-full fixed object-cover -z-10 top-0 left-0 pointer-events-none fade-in-card" src="wavebg2.svg"/>
             <div className="h-full w-full fixed object-cover z-[-11] top-0 left-0 loginBackgroundColor" />
 
-            <div className="border py-6 px-12 min-w-[280px] rounded-md shadow-lg shadow-gray-700 bg-white">
-                <img className="mx-auto mb-5" width={180} src="/cfdviewerlogo.svg" alt="CFD Viewer logo" />
+            <div className="border py-6 px-6 sm:px-12 min-w-[240px] rounded-md shadow-lg shadow-gray-700 bg-white">
+                <img className="mx-auto mb-5" width={isMobile ? 140 : 170} src="/cfdviewerlogo.svg" alt="CFD Viewer logo" />
                 <form onSubmit={loginClicked} className="flex flex-col space-y-3 w-full">
                     <h3 className="text-center text-[20px] font-semibold mb-2">Sign in</h3>
                     <CustomInput currentValue={username} labelText="username" onChange={(e:any) => setUsername(e.target.value)} />
                     <div className="w-full flex">
-                        <div className="w-[176px]"><CustomInput currentValue={password} labelText="password" onChange={(e:any) => setPassword(e.target.value)} showPassword={showPassword} /></div>
+                        <CustomInput currentValue={password} labelText="password" onChange={(e:any) => setPassword(e.target.value)} showPassword={showPassword} />
                         <div className="bg-gray-200 border-b-2 border-emerald-700 px-1 flex items-center">
                             {showPassword
                             ? <svg onClick={() => setShowPassword(false)} stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M12 19c.946 0 1.81-.103 2.598-.281l-1.757-1.757C12.568 16.983 12.291 17 12 17c-5.351 0-7.424-3.846-7.926-5 .204-.47.674-1.381 1.508-2.297L4.184 8.305c-1.538 1.667-2.121 3.346-2.132 3.379-.069.205-.069.428 0 .633C2.073 12.383 4.367 19 12 19zM12 5c-1.837 0-3.346.396-4.604.981L3.707 2.293 2.293 3.707l18 18 1.414-1.414-3.319-3.319c2.614-1.951 3.547-4.615 3.561-4.657.069-.205.069-.428 0-.633C21.927 11.617 19.633 5 12 5zM16.972 15.558l-2.28-2.28C14.882 12.888 15 12.459 15 12c0-1.641-1.359-3-3-3-.459 0-.888.118-1.277.309L8.915 7.501C9.796 7.193 10.814 7 12 7c5.351 0 7.424 3.846 7.926 5C19.624 12.692 18.76 14.342 16.972 15.558z"></path></svg>
