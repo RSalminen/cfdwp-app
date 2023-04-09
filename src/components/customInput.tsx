61310c015f7230b59f1c0fcbbb1eb6aab0b78b29
RSalminen <robin.salminen@aalto.fi>
2023-04-09 21:18:03 +0300

    improve login screen on mobile

--- src/components/customInput.tsx
+++ src/components/customInput.tsx
@@ -12,7 +12,7 @@ const CustomInput = ({labelText, onChange, currentValue, showPassword}:{labelTex
     return (
         <div className="flex flex-col border-b-2 border-emerald-700 bg-gray-200 pb-1 pt-0.5 rounded-t-sm w-full">
             <h5 className="text-[12px] text-emerald-800 pl-2">{labelText}</h5>
-            <input className="text-[13px] focus:outline-none bg-gray-200 px-1.5" type={showPassword !== false ? "text" : "password"} onKeyPress={stopPropagation} onKeyUp={stopPropagation} onKeyDown={stopPropagation} onChange={valueChanged} value={currentValue} />
+            <input className="text-[13px] focus:outline-none bg-gray-200 px-1.5 w-full min-w-[100px]" type={showPassword !== false ? "text" : "password"} onKeyPress={stopPropagation} onKeyUp={stopPropagation} onKeyDown={stopPropagation} onChange={valueChanged} value={currentValue} />
         </div>
     )
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -46,7 +46,7 @@ const Login = () => {
             <img role="presentation" className="h-full w-full fixed object-cover -z-10 top-0 left-0 pointer-events-none fade-in-card" src="wavebg2.svg"/>
             <div className="h-full w-full fixed object-cover z-[-11] top-0 left-0 loginBackgroundColor" />
 
-            <div className="border py-6 px-6 sm:px-12 min-w-[240px] rounded-md shadow-lg shadow-gray-700 bg-white">
+            <div className="border py-6 px-12 min-w-[240px] rounded-md shadow-lg shadow-gray-700 bg-white">
                 <img className="mx-auto mb-5" width={isMobile ? 140 : 170} src="/cfdviewerlogo.svg" alt="CFD Viewer logo" />
                 <form onSubmit={loginClicked} className="flex flex-col space-y-3 w-full">
                     <h3 className="text-center text-[20px] font-semibold mb-2">Sign in</h3>
