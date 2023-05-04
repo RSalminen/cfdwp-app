09eae926f061aed42d4bfc3a8cb75f325882774a
RSalminen <robin.salminen@aalto.fi>
2023-05-04 14:09:15 +0300

    Change loading screen

--- src/components/viewerLoadingScreen.tsx
+++ src/components/viewerLoadingScreen.tsx
@@ -1,13 +1,11 @@
-import LoadingPicture from '../assets/loading.svg';
-
 const ViewerLoadingScreen = ({loadProgress} : { loadProgress:number}) => {
 
     return (
       <div className="flex flex-col absolute z-[99] w-full h-full bg-black justify-center items-center text-white">
         <h4 className="text-semibold text-[24px]">Loading</h4>
-        <div className="border-b mt-16 relative w-[80%] max-w-[600px] bg-black">
-          <img className="w-full" src={LoadingPicture} alt="Loading bar" />
-          <div style={{width: `${100-loadProgress}%`}} className={`absolute h-full top-0 right-0 bg-black`}></div>
+        <div className="mt-16 relative w-[80%] max-w-[600px] bg-black">
+          <div className='w-full h-2 bg-emerald-900 rounded-sm'></div>
+          <div style={{width: `${100-loadProgress}%`}} className={`absolute h-full top-0 right-0 bg-gray-700 rounded-sm`}></div>
         </div>
         <p className="my-2">{loadProgress}%</p>
       </div>
