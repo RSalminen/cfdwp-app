7d2434f6b9af7530a21920b249be094fa1031453
Robin Salminen <robin765.salminen765@gmail.com>
2023-05-14 17:02:05 +0300

    update server

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 let apiUrl : string | undefined;
 
 if (process.env.NODE_ENV === "production") {
-    apiUrl="https://api-cfdwp.onrender.com"
+    apiUrl="https://cfdwp-server.onrender.com"
 } else {
     apiUrl=""
 }
